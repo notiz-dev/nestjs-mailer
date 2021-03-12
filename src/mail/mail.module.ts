7d6f96be04a026d1c0b14a4b7a6eaa8b7492a89d
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-03-12 13:07:57 +0100

    add handlebars adapter and mail service

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,5 +1,8 @@
 import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { Module } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -18,7 +21,16 @@ import { Module } from '@nestjs/common';
       defaults: {
         from: '"No Reply" <noreply@example.com>',
       },
+      template: {
+        dir: join(__dirname, 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
     }),
   ],
+  providers: [MailService],
+  exports: [MailService],
 })
 export class MailModule {}
---
+++ src/mail/mail.service.ts
@@ -0,0 +1,7 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class MailService {
+  constructor(private mailerService: MailerService) {}
+}
