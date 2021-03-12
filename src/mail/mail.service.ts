7f2409af62062925f27f76b787568e0fd2759e07
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-03-12 15:10:48 +0100

    user confirmation email

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,7 +1,23 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { User } from './../user/user.entity';
 
 @Injectable()
 export class MailService {
   constructor(private mailerService: MailerService) {}
+
+  async sendUserConfirmation(user: User, token: string) {
+    const url = `example.com/auth/confirm?token=${token}`;
+
+    await this.mailerService.sendMail({
+      to: user.email,
+      // from: '"Support Team" <support@example.com>', // override default from
+      subject: 'Welcome to App. Confirm your Email',
+      template: 'confirmation', // `.hbs` extension is appended automatically
+      context: {
+        name: user.name,
+        url,
+      },
+    });
+  }
 }
