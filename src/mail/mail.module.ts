eaadfac66e8a500776f6b3ee62473a6eb2a2d40b
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-03-14 20:22:21 +0100

    fix mail transport and add configmodule optional import

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -3,17 +3,18 @@ import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handleba
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { join } from 'path';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     MailerModule.forRootAsync({
+      // imports: [ConfigModule], // import module if not enabled globally
       useFactory: async (config: ConfigService) => ({
         // transport: config.get("MAIL_TRANSPORT"),
         // or
         transport: {
           host: config.get('MAIL_HOST'),
-          secure: 'false',
+          secure: false,
           auth: {
             user: config.get('MAIL_USER'),
             pass: config.get('MAIL_PASSWORD'),
