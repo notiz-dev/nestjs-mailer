9ba6eb7479fa19ef476b393e6c022047bd64dfc3
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2021-08-16 18:01:49 +0200

    add global decorator

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,10 +1,11 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { join } from 'path';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
+@Global() // 👈 optional to make module global
 @Module({
   imports: [
     MailerModule.forRootAsync({
